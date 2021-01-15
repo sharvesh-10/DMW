7b9e87111e1698cf19d47399261fda095152d5fd
sharvesh-10 <sharveshsofficial@gmail.com>
2021-01-15 23:49:40 +0530

    minor change

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,8 +5,7 @@ import userReducer from './user/user-reducer';
 
 const persistConfig ={
     key: 'root',
-    storage,
-    whitelist: ['cart']   
+    storage, 
 }
 const rootReducer =combineReducers({
     user: userReducer,
